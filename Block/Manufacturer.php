d68c29864611e2356c6e65e3ffdd989272bb5b48
Charchit Khandelwal <charchit@newgenpayments.com>
2020-07-17 13:53:12 +0530

    [Update] link for manufacturer products
    Signed-off-by: Charchit Khandelwal <charchit@newgenpayments.com>

--- Block/Manufacturer.php
+++ Block/Manufacturer.php
@@ -10,24 +10,36 @@ class Manufacturer extends \Magento\Framework\View\Element\Template
 
     protected $_storeManager;
 
+    protected $urlBuilder;
+
     public function __construct(
         \Magento\Catalog\Model\ResourceModel\Product\Collection $productRepo,
         \Magento\Catalog\Block\Product\AbstractProduct $productClass,
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\UrlInterface $urlBuilder,
         array $data = []
     ) {
         $this->_productRepo = $productRepo;
         $this->_productClass = $productClass;
         $this->_storeManager = $storeManager;
+        $this->_urlBuilder = $urlBuilder;
         parent::__construct($context, $data);
     }
 
-    private function getManufacturer()
+    public function getManufacturer()
     {
         $product = $this->_productClass->getProduct();
         $manufacturerName = $product->getAttributeText('manufacturer');
         return $manufacturerName;
+    }
+
+    public function getManufacturerProductsLink($queryParams)
+    {
+        $query = [
+            'q' => $queryParams,
+        ];
+        return $this->_urlBuilder->getUrl('catalogsearch/result/index', ['_query' => $query]);
     }
 
     private function getStoreInfo()
